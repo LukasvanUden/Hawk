aa86b041e013a43f98e7637d3efcc22dd13b8921
Amit Dutta <amitdutta4255@gmail.com>
2026-05-13 07:46:46 +0530

    Update service worker to include full site link

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 const CACHE_NAME = 'walogger-v1';
 const ASSETS = [
     './',
-    './index.html',
+    '<-- full site link -->',
     './icon.svg',
     'https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&family=Google+Sans:wght@400;500;700&display=swap',
     'https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,400,0,0'
